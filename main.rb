bc5d661d222a86a6d5fb8d10376d2063e29dc95e
Max Whit <symbolt.ap@gmail.com>
2018-08-16 11:05:07 -0600

    update main.rb

---
+++ main.rb
@@ -0,0 +1,26 @@
+
+
+require 'colorize'
+def puts_git(cmd)
+  puts 'git #{cmd} -h'
+  menu
+end
+
+def menu
+  puts '1: Enter Git command'.colorize:red
+  puts '2: Exit'.colorize:red
+  choice = gets.to_i
+  case choice
+  when 1
+    puts 'Enter Git Command'.colorize:red
+    puts_git(gets.strip)
+    menu
+  when 2
+    exit(0)
+  else 
+    puts 'Invalid choice'.colorize:red
+    menu
+  end
+end
+
+menu
